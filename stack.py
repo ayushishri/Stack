97ea810d27ace365de19f2eb8de4d2b6ace08fcb
ayushishri <32212671+ayushishri@users.noreply.github.com>
2018-09-06 20:44:09 -0400

    stack

--- stack.py
+++ stack.py
@@ -1,1 +1,9 @@
-
+class stack:
+	def __init__(self):
+		self.store=[]
+	def push(self,val):
+		self.store+=[val]
+		return 0
+	def pop(self):
+		self.store=self.store[:len(self.store)-1]
+		return 0
