b85b6e1446270d5605916e51abcdd4a4c969afdb
ayushishri <32212671+ayushishri@users.noreply.github.com>
2018-09-06 21:27:11 -0400

    stack

--- stack.py
+++ stack.py
@@ -1,9 +1,20 @@
 class stack:
 	def __init__(self):
 		self.store=[]
+		self.count=0
+
 	def push(self,val):
 		self.store+=[val]
+		self.count+=1
 		return 0
+
 	def pop(self):
+		rval=self.store[len(self.store)-1]
 		self.store=self.store[:len(self.store)-1]
-		return 0
+		self.count-=1
+		return rval
+
+	def isEmpty(self):
+		if (self.count==0):
+			return True
+		return False
